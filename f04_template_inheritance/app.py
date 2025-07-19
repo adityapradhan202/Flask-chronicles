66c17cf4a0a5fe4bafe2aa36a49f3695f86885da
Aditya Pradhan <adityapradhan5060@gmail.com>
2025-07-19 18:39:23 +0530

    variable rule in flask

--- f04_template_inheritance/app.py
+++ f04_template_inheritance/app.py
@@ -28,6 +28,20 @@ def movies():
 def creepy():
     return render_template("creepy.html", users=users[0])
 
+# There's one concept called the variable rule
+# Variable rule!
+# We can add variable section to the url
+# And then a variable can be appended to the url section
+# This makes this variable accessible inside the function
+# To read more about this refer to documentation
+
+@app.route("/user_profile/<string:user_name>") # user_name becomes the function param
+def show_user_profile(user_name):
+    return render_template("user_profile.html", user_name=user_name)
+
+# In the above function the string is the type of user_name
+# There are different types that we can mention, read more in official docs
+
 if __name__ == "__main__":
     app.run(debug=True)
 
